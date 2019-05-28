a5516795b79cb91a2ed4c7b39b14a88d01b55b69
q <fire@qq.com>
2019-05-28 08:13:05 +0800

    qiu 19-5-28 08:12

--- src/main/java/com/Educational/controller/LoginController.java
+++ src/main/java/com/Educational/controller/LoginController.java
@@ -5,9 +5,12 @@ import com.Educational.entity.StudentInfo;
 import com.Educational.service.StaffInfoService;
 import com.Educational.service.StudentInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -18,6 +21,7 @@ import javax.servlet.http.HttpSession;
  * @create 2019- 05- 26 23:27
  */
 
+@Controller
 public class LoginController {
 
 
@@ -28,30 +32,34 @@ public class LoginController {
 
     //登录功能
     @ResponseBody
-    @RequestMapping("/login")
-    public String  login(Integer type, String name, String password, HttpSession session){
-
+    @RequestMapping(value = "/login",method = RequestMethod.POST,produces={"application/json;charset=utf-8"})
+    public String  login(Integer type, String name, String pass, HttpSession session, HttpServletResponse response){
+        response.setContentType("application/json;charset=utf-8");
+        System.out.println(name+"-------------------------"+pass);
         if(type == 1){//1为老师登录否者学生登录
             StaffInfo staffInfo=new StaffInfo();
             staffInfo.setUserNumber(name);
-            staffInfo.setUserPassowrd(password);
+            staffInfo.setUserPassowrd(pass);
             StaffInfo staffInfo1queryOne = staffInfoService.queryOne(staffInfo);
+
             if(staffInfo1queryOne!=null) {//登录成功
+
                 session.setAttribute("staffInfo",staffInfo1queryOne);
-                return "loginok";
+                System.out.println(staffInfo1queryOne);
+                return staffInfo1queryOne.toString();
             } else{
-                return "loginerror";
+                return null;
             }
         }else {
             StudentInfo studentInfo = new StudentInfo();
             studentInfo.setStudentNo(name);//学生以学号登录
-            studentInfo.setStudentPassword(password);
+            studentInfo.setStudentPassword(pass);
             StudentInfo studentInfoqueryOne = studentInfoService.queryOne(studentInfo);
             if (studentInfoqueryOne != null) {//登录成功
                 session.setAttribute("studentInfo",studentInfoqueryOne);
-                return "loginok";
+                return studentInfoqueryOne.toString();
             } else {
-                return "loginerror";
+                return null;
             }
 
         }
--- src/main/java/com/Educational/filter/VueFilter.java
+++ src/main/java/com/Educational/filter/VueFilter.java
@@ -3,6 +3,7 @@ package com.Educational.filter;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * @outhor Mr.Huang
@@ -17,7 +18,14 @@ public class VueFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
             HttpServletResponse response = (HttpServletResponse) servletResponse;
-            response.setHeader("Access-Control-Allow-Origin", "*");
+        //设置response对数据的编码方式
+        response.setContentType("application/json;charset=utf-8");
+        //告诉浏览器解码方式
+        response.setHeader("content-type", "text/html;charset=utf-8");
+
+
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
             response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
             response.setHeader("Access-Control-Max-Age", "3600");
             response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
